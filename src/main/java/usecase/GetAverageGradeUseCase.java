d786bc9fd66e8ea987669ea8c4627391dbc6f41e
cormacyounguoft <cormac.young@mail.utoronto.ca>
2024-09-30 12:13:56 -0400

    Task 3a

--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -4,6 +4,9 @@ import api.GradeDataBase;
 import entity.Grade;
 import entity.Team;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * GetAverageGradeUseCase class.
  */
@@ -28,6 +31,17 @@ public final class GetAverageGradeUseCase {
         // Call the API to get all the grades for the course for all your team members
         // TODO Task 3a: Complete the logic of calculating the average course grade for
         //              your team members. Hint: the getGrades method might be useful.
+
+        List<Grade> grades = new ArrayList<Grade>();
+
+        for (String member : team.getMembers()) {
+            grades.add(gradeDataBase.getGrade(member, course));
+        }
+
+        for (Grade grade : grades) {
+            sum += grade.getGrade();
+            count++;
+        }
 
         if (count == 0) {
             return 0;
